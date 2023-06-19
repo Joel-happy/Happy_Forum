8a5236f088d31bff2ab3e0491d5f72bf3b523a7f
JOEL <joelhappy16@gmail.com>
2023-06-20 01:22:56 +0200

    mise en place des fonctionnalités de connexion et d'inscription

--- backend/main.go
+++ backend/main.go
@@ -2,45 +2,67 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB // Déclarer une variable globale pour la connexion à la base de données
+
 func main() {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:8080)/projetforum")
+	// Servir les fichiers statiques du répertoire "frontend"
+	fs := http.FileServer(http.Dir("frontend"))
+	http.Handle("/frontend/", http.StripPrefix("/frontend/", fs))
+
+	// Chaîne de connexion à la base de données MySQL
+	connectionString := "root:@tcp(localhost:3306)/projetforum"
+
+	// Ouvrir une connexion à la base de données
+	var err error
+	db, err = sql.Open("mysql", connectionString) // Assigner la connexion ouverte à la variable globale db
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	router := http.NewServeMux()
+	// Vérifier la connexion à la base de données
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	router.HandleFunc("/inscription", handleInscription)
-	router.HandleFunc("/connexion", handleConnexion)
+	// Créer un routeur Gin
+	router := gin.Default()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	// Charger les fichiers HTML du répertoire "frontend"
+	router.LoadHTMLGlob(`..\frontend\*.html`)
+
+	router.POST("/inscription", handleInscription)
+	router.POST("/connexion", handleConnexion)
+	router.GET("/accueil", handleAccueil)
+
+	// Démarrer le serveur HTTP
+	router.Run(":8080")
 }
 
-func handleInscription(w http.ResponseWriter, r *http.Request) {
+func handleInscription(c *gin.Context) {
 	// Récupérer les données du formulaire d'inscription
-	username := r.FormValue("username")
-	email := r.FormValue("email")
-	password := r.FormValue("password")
+	username := c.PostForm("username")
+	email := c.PostForm("email")
+	password := c.PostForm("password")
 
 	// Vérifier si les données sont dupliquées
 	isDuplicate, err := verification(username, email)
 	if err != nil {
 		// Gérer l'erreur
-		http.Error(w, "Une erreur s'est produite lors de la vérification des données", http.StatusInternalServerError)
+		c.String(http.StatusInternalServerError, "Une erreur s'est produite lors de la vérification des données")
 		return
 	}
 
 	if isDuplicate {
 		// Afficher un message d'erreur indiquant que les données sont déjà utilisées
-		fmt.Fprintf(w, "Le pseudo ou l'adresse e-mail est déjà utilisé(e)")
+		c.String(http.StatusBadRequest, "Le pseudo ou l'adresse e-mail est déjà utilisé(e)")
 		return
 	}
 
@@ -48,49 +70,28 @@ func handleInscription(w http.ResponseWriter, r *http.Request) {
 	err = insertUser(username, email, password)
 	if err != nil {
 		// Gérer l'erreur
-		http.Error(w, "Une erreur s'est produite lors de l'inscription", http.StatusInternalServerError)
+		c.String(http.StatusInternalServerError, "Une erreur s'est produite lors de l'inscription")
 		return
 	}
 
 	// Afficher un message de confirmation
-	fmt.Fprintf(w, "Vous êtes maintenant inscrit sur le forum")
-
-	// Redirection ou réponse en fonction du résultat de l'inscription
-	// ...
+	c.String(http.StatusOK, "Vous êtes maintenant inscrit sur le forum")
 }
 
 func verification(username string, email string) (bool, error) {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:8080)/projetforum")
-	if err != nil {
-		return false, err
-	}
-	defer db.Close()
-
 	query := "SELECT COUNT(*) FROM utilisateur WHERE Pseudo = ? OR Adresse_e_mail = ?"
 	var count int
-	err = db.QueryRow(query, username, email).Scan(&count)
+	err := db.QueryRow(query, username, email).Scan(&count)
 	if err != nil {
 		return false, err
 	}
 
-	if count > 0 {
-		// Les données sont déjà présentes dans la base de données
-		return true, nil
-	}
-
-	// Les données sont uniques
-	return false, nil
+	return count > 0, nil
 }
 
 func insertUser(username string, email string, password string) error {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:8080)/projetforum")
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
 	query := "INSERT INTO utilisateur (Pseudo, Adresse_e_mail, Mot_de_passe) VALUES (?, ?, ?)"
-	_, err = db.Exec(query, username, email, password)
+	_, err := db.Exec(query, username, email, password)
 	if err != nil {
 		return err
 	}
@@ -99,15 +100,9 @@ func insertUser(username string, email string, password string) error {
 }
 
 func checkCredentials(username string, password string) (bool, error) {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:8080)/projetforum")
-	if err != nil {
-		return false, err
-	}
-	defer db.Close()
-
 	query := "SELECT COUNT(*) FROM utilisateur WHERE Pseudo = ? AND Mot_de_passe = ?"
 	var count int
-	err = db.QueryRow(query, username, password).Scan(&count)
+	err := db.QueryRow(query, username, password).Scan(&count)
 	if err != nil {
 		return false, err
 	}
@@ -115,24 +110,29 @@ func checkCredentials(username string, password string) (bool, error) {
 	return count == 1, nil
 }
 
-func handleConnexion(w http.ResponseWriter, r *http.Request) {
+func handleConnexion(c *gin.Context) {
 	// Récupérer les données du formulaire de connexion
-	username := r.FormValue("username")
-	password := r.FormValue("password")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
 
 	// Vérifier les informations de connexion
 	isValid, err := checkCredentials(username, password)
 	if err != nil {
 		// Gérer l'erreur
-		http.Error(w, "Une erreur s'est produite lors de la vérification des informations de connexion", http.StatusInternalServerError)
+		c.String(http.StatusInternalServerError, "Une erreur s'est produite lors de la vérification des informations de connexion")
 		return
 	}
 
 	if isValid {
 		// Connexion réussie, rediriger vers la page d'accueil par exemple
-		http.Redirect(w, r, "/accueil", http.StatusFound)
+		c.Redirect(http.StatusFound, "/frontend/accueil.html")
 	} else {
 		// Connexion échouée, afficher un message d'erreur
-		fmt.Fprintf(w, "Identifiant ou mot de passe incorrect")
+		c.String(http.StatusUnauthorized, "Identifiant ou mot de passe incorrect")
 	}
+}
+
+func handleAccueil(c *gin.Context) {
+	// Charger et afficher la page d'accueil
+	c.HTML(http.StatusOK, "accueil.html", nil)
 }
